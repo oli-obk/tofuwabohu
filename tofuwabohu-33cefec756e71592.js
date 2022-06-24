298d85a66681d5e0fe86c62be992ab1b915845ac
oli-obk <oli-obk@users.noreply.github.com>
2022-06-24 21:24:18 +0000

    deploy: bd11dd5192e5ed007774cf2cf02324bcd14db04d

--- tofuwabohu-33cefec756e71592.js
+++ tofuwabohu-33cefec756e71592.js
@@ -66,7 +66,7 @@ function initSync(bytes) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('tofuwabohu-2602bd4387dec7ec_bg.wasm', import.meta.url);
+        input = new URL('tofuwabohu-33cefec756e71592_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
