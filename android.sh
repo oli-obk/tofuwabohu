c21c32f6bb88be37c2547bd4eb42574c3fe89656
Oli Scherer <github35764891676564198441@oli-obk.de>
2022-06-26 09:37:54 +0200

    Fix android build

--- android.sh
+++ android.sh
@@ -1,2 +1,3 @@
-rustup target add armv7-linux-androideabi aarch64-linux-android i686-linux-android thumbv7neon-linux-androideabi x86_64-linux-android 
+rustup target add armv7-linux-androideabi aarch64-linux-android i686-linux-android thumbv7neon-linux-androideabi x86_64-linux-android
+rm -r /usr/local/cargo/registry # workaround for https://github.com/not-fl3/macroquad/issues/400
 cargo quad-apk build --release
